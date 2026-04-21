a1ff99b5f6ea8a218074ed3e212f6774e6f3aa01
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-04-21 21:59:36 +0000

    alan 21 abril

---
+++ naming-variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    int number; //i fixed this by removing the exclamation mark
+int test_value; //created space with an underscore
+int example1; //number 1 was first but that cant be so i passed it to the front
+float alan; //fixed the name because its not "int"
+printf("Variables created successfully.\n");
+}
