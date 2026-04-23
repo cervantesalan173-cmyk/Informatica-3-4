62dee7d6a90fc4be12198e9fdd3e35b6319810d9
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-04-23 15:48:09 +0000

    Vending

---
+++ Vending.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void){
+    printf("Vending Machine Stock\n");
+    printf("---------------------------\n");
+    // Create variables for first snack (Quesadilla)
+    float price1= 99.99;
+    int Quantity1= 12;
+    char Selection_code1 = 'A';
+    // Create variables for second snack (Quesabirria)
+    float price2= 160.50;
+    int Quantity2= 16;
+    char Selection_code2 = 'B';
+    // Create variables for third snack (Quesicarne)
+    float price3= 120.99;
+    int Quantity3= 8;
+    char Selection_code3 = 'C';
+    printf("Item name: Quesadilla\n");
+    printf("Price = %.2f \n",price1);
+    
+
+}
