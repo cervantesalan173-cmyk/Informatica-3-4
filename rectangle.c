3d8ba505e7a3f012a55b3c643b68f56859026a65
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-04-24 17:30:25 +0000

    rectangle

---
+++ rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int lenght;
+    int width;
+    int area;
+    int perimeter;
+    printf("enter lenght: ");
+    scanf("%d", &lenght);
+    printf("enter width: ");
+    scanf("%d", &width);
+    area = lenght * width;
+    perimeter = 2*(length + width);
+}
+
