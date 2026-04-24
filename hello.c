06948fd94e8fff6a2de5767c2c8a110c57f3aff3
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-04-24 14:58:59 +0000

    faccolodr

--- hello.c
+++ hello.c
@@ -2,5 +2,17 @@
 
 int main(void)
 {
-printf("Hello Alan! \n");
+
+char Name[67];
+printf("Your Name: ");
+scanf("%s", &Name);
+printf("Your name is: %s \n",Name);
+ char Color[67];
+ printf("Your favcolor: ");
+scanf("%s", &Color);
+printf("Your favcolor is: %s \n",Color);
+
+
 }
+
+//printf("Hello Alan! \n");
