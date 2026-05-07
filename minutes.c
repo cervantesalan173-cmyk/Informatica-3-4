48ce305fed2fdeaad80ead8e942b624bc8003880
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-05-07 19:24:34 +0000

    minutes

--- minutes.c
+++ minutes.c
@@ -11,7 +11,11 @@ int hours = (runningtime / 60);
 int minutes = (runningtime % 60);
 int endhour = starthour + hours;
 int endmin = startmin + minutes;
+if (endmin >= 60){
+    endhour = endhour + (endmin / 60); //update value of endhour variable
+    endmin = endmin % 60;              //update the value of endmin varibale
+}
 printf("The movie will last %d hr and %d min\n", hours, minutes);
-printf("End time: %d:%d\n", endhour, endmin);
+printf("End time: %d:%02d\n", endhour, endmin);
 
 }
